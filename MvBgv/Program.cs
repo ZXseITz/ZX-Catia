b2078b94e8e30a05580ff7011c940e0fe33fee8c
Claudio Seitz <claudio.seitz@students.fhnw.ch>
2016-04-08 13:56:41 +0200

    Added function: change main view - background view

--- MvBgv/Program.cs
+++ MvBgv/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CATmain;
+using DRAFTINGITF;
+using System.Windows.Forms;
 
 namespace MvBgv
 {
@@ -10,6 +13,29 @@ namespace MvBgv
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string[] filter = { CATB.filterDraw };
+                CATB catia = new CATB(filter);
+
+                DrawingSheet aSheet = ((DrawingDocument)catia.getActiveDoc()).Sheets.ActiveSheet;
+                // change view
+                if (aSheet.Views.ActiveView != aSheet.Views.Item(2))
+                {
+                    //background
+                    aSheet.Views.Item(2).Activate();
+                }
+                else
+                {
+                    //main view
+                    aSheet.Views.Item(1).Activate();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Main view - Background view", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Environment.Exit(0);
+            }
         }
     }
 }
