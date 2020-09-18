de1cc90982c00349f22e131d098a465a175e3494
Claudio Seitz <zxseitz@gmail.com>
2020-09-18 13:49:17 +0200

    refactored CATmain and FgBgv

---
+++ FgBgv/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using CATmain;
+using DRAFTINGITF;
+using System.Windows.Forms;
+
+namespace FgBgv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string caption = "Foreground - Background view";
+            const int foreground = 1;
+            const int background = 2;
+            try
+            {
+                var catia = Bootstrap.Init();
+                var activeDoc = catia.ActiveDocument;
+                if (!Bootstrap.CheckDocument(activeDoc, DocType.Drawing))
+                {
+                    MessageBox.Show("Expected drawing document", caption,
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+
+                var activeSheet = ((DrawingDocument) activeDoc).Sheets.ActiveSheet; // Cannot be null
+                var views = activeSheet.Views;
+                views.Item(views.ActiveView == views.Item(foreground) ? background : foreground).Activate();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
